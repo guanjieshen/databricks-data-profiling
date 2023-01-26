59a393488dcc1efa0ab14ac3f73c8502a502bee7
guanjieshen <guanjieshen>
2023-01-26 01:31:28 +0000

    Add comment

--- [Run First] Profiling Tool Setup.py
+++ [Run First] Profiling Tool Setup.py
@@ -1,3 +1,7 @@
 # Databricks notebook source
+# MAGIC %md ### Create Default Table in HMS
+
+# COMMAND ----------
+
 df = spark.range(1,10)
 df.write.format("delta").mode("overwrite").saveAsTable("default.default")
