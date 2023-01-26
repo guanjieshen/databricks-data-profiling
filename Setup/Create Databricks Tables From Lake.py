9d1f653396ddf2ac25c4c4ab453a413b37c2ae20
guanjieshen <guanjieshen>
2023-01-26 21:10:47 +0000

    Add new folder structure

---
+++ Setup/Create Databricks Tables From Lake.py
@@ -0,0 +1,30 @@
+# Databricks notebook source
+# MAGIC %md #### Create from Delta Files
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md #### Create from Parquet Files
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md #### Create from JSON Files
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md #### Create from CSV Files
+
+# COMMAND ----------
+
+
